a37a7dbb56f670038ff23350f2fef9a4403f92ef
Katerina Savidaki <ksavvidaki@gmail.com>
2025-05-20 21:01:32 +0300

    feat: new components created

---
+++ src/components/FunctionalComponent.tsx
@@ -0,0 +1,7 @@
+function FunctionComponent() {
+    const title = "Is a Functional Component";
+
+    return <h1 className="text-center mt-12 text-xl font-bold">{title}</h1>
+}
+
+export default FunctionComponent;
