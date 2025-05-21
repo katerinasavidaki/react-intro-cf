640b0397c814e08f10f3c18ed9b4ba92639bd9f5
Katerina Savidaki <ksavvidaki@gmail.com>
2025-05-21 20:01:39 +0300

    feat: using states in components

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,32 @@
-import ClassComponent from "./components/ClassComponent.tsx";
-import FunctionalComponent from "./components/FunctionalComponent.tsx";
-import ArrowFunctionalComponent from "./components/ArrowFunctionalComponent.tsx";
-import ArrowFunctionalComponentWithProps from "./components/ArrowFunctionalComponentWithProps.tsx";
-import ArrowFunctionalComponentWithPropsType from "./components/ArrowFunctionalComponentWithPropsType.tsx";
+// import ClassComponent from "./components/ClassComponent.tsx";
+// import FunctionalComponent from "./components/FunctionalComponent.tsx";
+// import ArrowFunctionalComponent from "./components/ArrowFunctionalComponent.tsx";
+// import ArrowFunctionalComponentWithProps from "./components/ArrowFunctionalComponentWithProps.tsx";
+// import ArrowFunctionalComponentWithPropsType from "./components/ArrowFunctionalComponentWithPropsType.tsx";
+
+import Layout from "./components/Layout.tsx";
+import FunctionalComponentWithState from "./components/FunctionalComponentWithState.tsx";
+// import ClassComponentWithState from "./components/ClassComponentWithState.tsx";
+
 
 function App() {
 
   return (
     <>
-        <ClassComponent />
-        <FunctionalComponent />
-        <ArrowFunctionalComponent />
-        <ArrowFunctionalComponentWithProps title="Is an Arrow Functional Component With Props"/>
-        <ArrowFunctionalComponentWithPropsType
-            title="Is an Arrow Functional Component With Props"
-            description="this is a description"/>
+        <Layout>
+            {/*<ClassComponent />*/}
+            {/*<FunctionalComponent />*/}
+            {/*<ArrowFunctionalComponent />*/}
+            {/*<ArrowFunctionalComponentWithProps title="Is an Arrow Functional Component With Props"/>*/}
+            {/*<ArrowFunctionalComponentWithPropsType*/}
+            {/*    title="Is an Arrow Functional Component With Props"*/}
+            {/*    description="this is a description"/>*/}
+
+            {/*<ClassComponentWithState/>*/}
+            <FunctionalComponentWithState/>
+
+        </Layout>
+
     </>
   )
 }
