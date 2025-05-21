0157f0e841c3f6efbeb9b6a149bc18aa402c86f6
Katerina Savidaki <ksavvidaki@gmail.com>
2025-05-21 20:00:35 +0300

    feat: add cf logo

---
+++ src/components/CodingFactoryLogo.tsx
@@ -0,0 +1,13 @@
+const CodingFactoryLogo = () => {
+    return(
+        <>
+            <img
+                src="/logo.png"
+                alt="CF7"
+                className="h-16 my-4"
+            />
+        </>
+    )
+}
+
+export default CodingFactoryLogo;
