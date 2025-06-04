6b8e8ac45e7ee2e5c9b7cff1620e24e481c43285
Katerina Savidaki <ksavvidaki@gmail.com>
2025-06-04 19:59:44 +0300

    feat: add router layout

--- src/App.tsx
+++ src/App.tsx
@@ -22,9 +22,10 @@
 import {BrowserRouter, Route, Routes} from "react-router";
 import HomePage from "./pages/HomePage.tsx";
 import NameChangerPage from "./pages/NameChangerPage.tsx";
-import Layout from "./components/Layout.tsx";
+// import Layout from "./components/Layout.tsx";
 import OnlineStatusPage from "./pages/OnlineStatusPage.tsx";
 import UserPage from "./pages/UserPage.tsx";
+import RouterLayout from "./components/RouterLayout.tsx";
 
 function App() {
 
@@ -60,21 +61,24 @@ function App() {
         {/*</Layout>*/}
 
         <BrowserRouter>
-            <Layout>
+            {/*<Layout>*/}
                 <Routes>
                     {/*<Route path="/" element={<HomePage/>}/>*/}
-                    <Route index element={<HomePage/>}/>
-                    <Route path="name-changer" element={<NameChangerPage/>}/>
+                    <Route element={<RouterLayout/>}>
+                        <Route index element={<HomePage/>}/>
+                    </Route>
 
+                    <Route path="name-changer" element={<NameChangerPage/>}/>
                     <Route path="examples?">
                         <Route path="name-changer" element={<NameChangerPage/>}/>
                         <Route path="online-status" element={<OnlineStatusPage/>}/>
                     </Route>
-
                     <Route path="users/:userId" element={<UserPage/>}/>
+                    <Route path="users" element={<UserPage/>}/>
+                    {/*<Route path="files/*" element={<FilePage/>}/>*/}
 
                 </Routes>
-            </Layout>
+            {/*</Layout>*/}
         </BrowserRouter>
     </>
   )
