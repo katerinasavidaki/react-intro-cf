8ea4374ca199e8a3d630134b1c08280e18775993
Katerina Savidaki <ksavvidaki@gmail.com>
2025-06-16 18:04:45 +0300

    feat: add multifield form with zod validation

--- src/App.tsx
+++ src/App.tsx
@@ -34,7 +34,8 @@ import NotFoundPage from "./pages/NotFoundPage.tsx";
 import UncontrolledInput from "./components/UncontrolledInput.tsx";
 // import MultiFieldForm from "./components/MultiFieldForm.tsx";
 import ControlledInput from "./components/ControlledInput.tsx";
-import MultiFieldFormWithValidation from "./components/MultiFieldFormWithValidation.tsx";
+// import MultiFieldFormWithValidation from "./components/MultiFieldFormWithValidation.tsx";
+import MultiFieldFormWithZodValidation from "./components/MultiFieldFormWithZodValidation.tsx";
 
 function App() {
 
@@ -77,7 +78,8 @@ function App() {
                         {/*<Route index element={<HomePage/>}/>*/}
                         {/*<Route index element={<FocusInput/>}/>*/}
                         {/*<Route path="multifield-form" element={<MultiFieldForm/>}/>*/}
-                        <Route index element={<MultiFieldFormWithValidation/>}/>
+                        {/*<Route index element={<MultiFieldFormWithValidation/>}/>*/}
+                        <Route index element={<MultiFieldFormWithZodValidation/>}/>
                         {/*<Route index element={<UncontrolledInput/>}/>*/}
                         <Route path="users/:userId" element={<UserPage/>}/>
                         <Route path="users" element={<UserPage/>}/>
